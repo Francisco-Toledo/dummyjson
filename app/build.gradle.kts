37aabba3c02d412225ecaf0243df0ebe51c1fbc1
Francisco <franacisco.toledo@live.u-tad.com>
2025-06-23 16:54:56 +0200

    " commit 16:40"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.utad.dummyjson"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.utad.dummyjson"
@@ -51,9 +51,10 @@ android {
         androidTestImplementation(libs.androidx.junit)
         androidTestImplementation(libs.androidx.espresso.core)
 
+        implementation("androidx.core:core:8.6.0")
         implementation("androidx.core:core-ktx:1.13.1")
         implementation("androidx.appcompat:appcompat:1.7.0")
-        implementation("com.google.android.material:material:1.12.0")
+        implementation("com.google.android.material:material:1.10.0")
         implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
         // ---- UI / RecyclerView ----
